eae516b4c08e4200c85f4eaa480d18e2bccfd26b
Bingjia Wang <bw499@cornell.edu>
2020-04-23 16:15:28 -0400

    Add files via upload

---
+++ batch_sim_hotel_mgmt.sh
@@ -0,0 +1,10 @@
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 20 >  ./results/hotel_mgmt_20k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 40 >  ./results/hotel_mgmt_40k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 60 >  ./results/hotel_mgmt_60k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 80 >  ./results/hotel_mgmt_80k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 100 > ./results/hotel_mgmt_100k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 120 > ./results/hotel_mgmt_120k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 140 > ./results/hotel_mgmt_140k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 160 > ./results/hotel_mgmt_160k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 180 > ./results/hotel_mgmt_180k.txt & \
+./microsim ./architecture/hotel_mgmt/json/ 320 65000 expo 200 > ./results/hotel_mgmt_200k.txt &
